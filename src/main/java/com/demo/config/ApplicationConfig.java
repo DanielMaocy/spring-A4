bba6f728f568fe7661e3db10c371f6aa56d29e4b
maocy <345106328@qq.com>
2019-03-12 17:22:22 +0800

    Java Config

--- src/main/java/com/demo/config/ApplicationConfig.java
+++ src/main/java/com/demo/config/ApplicationConfig.java
@@ -1,10 +1,17 @@
 package com.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.demo.entity.Order;
+
 @Configuration
-@ComponentScan(value="com.demo.entity")
+//@ComponentScan(value="com.demo.entity")
 public class ApplicationConfig {
 
+	@Bean
+	public Order order() {
+		return new Order();
+	}
 }
