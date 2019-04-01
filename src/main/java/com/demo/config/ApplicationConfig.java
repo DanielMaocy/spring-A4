b38e636dba8c963a0dd9ae4855834f4263d82273
maocy <345106328@qq.com>
2019-04-01 10:53:36 +0800

    指定环境 profile

--- src/main/java/com/demo/config/ApplicationConfig.java
+++ src/main/java/com/demo/config/ApplicationConfig.java
@@ -1,8 +1,27 @@
 package com.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.demo.entity.Order;
 
 @Configuration
 public class ApplicationConfig {
 
+	@Bean
+	@Profile("dev")
+	public Order orderDev() {
+		Order order = new Order();
+		order.setOrderNo("DEV00001");
+		return order;
+	}
+	
+	@Bean
+	@Profile("prod")
+	public Order orderProd() {
+		Order order = new Order();
+		order.setOrderNo("PROD00001");
+		return order;
+	}
 }
--- src/test/java/com/demo/ApplicationTest.java
+++ src/test/java/com/demo/ApplicationTest.java
@@ -1,10 +1,15 @@
 package com.demo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.demo.config.ApplicationConfig;
+import com.demo.entity.Order;
+import com.demo.entity.Product;
 
 /**
  * 测试类
@@ -14,7 +19,24 @@ import com.demo.config.ApplicationConfig;
  * @Date: 2019年3月6日 下午2:57:03
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes= {ApplicationConfig.class})
+//@ContextConfiguration(classes= {ApplicationConfig.class})
+@ContextConfiguration("/spring.xml")
+@ActiveProfiles("prod")
 public class ApplicationTest {
 
+	@Autowired(required=false)
+	private Order order;
+	
+	@Autowired
+	private Product product;
+	
+	@Test
+	public void test() {
+		order.printOrderInfo();
+	}
+	
+	@Test
+	public void testProduct() {
+		product.printProductInfo();
+	}
 }
