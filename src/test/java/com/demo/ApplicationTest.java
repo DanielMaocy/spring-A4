81449530dfc30bf1dcbe5b29a14c2edd560e6dc6
maocy <345106328@qq.com>
2019-04-05 20:33:45 +0800

    Spring mvc

--- src/test/java/com/demo/ApplicationTest.java
+++ src/test/java/com/demo/ApplicationTest.java
@@ -1,13 +1,8 @@
 package com.demo;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.demo.service.PayService;
-import com.demo.service.RiskService;
 
 /**
  * 测试类
@@ -19,22 +14,4 @@ import com.demo.service.RiskService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/spring.xml")
 public class ApplicationTest {
-
-	@Autowired
-	private PayService rmbPayService;
-	
-	@Autowired
-	private PayService usdPayService;
-	
-	@Test
-	public void testRMBPayService() {
-		rmbPayService.payment("RMB00001");
-	}
-	
-	@Test
-	public void testUSDPayService() {
-		usdPayService.payment("USD00002");
-		RiskService riskService = (RiskService) usdPayService;
-		riskService.limitPayNum();
-	}
 }
